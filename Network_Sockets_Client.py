ee49cdbb02fdb9606a6fda9087b5ef38b3568482
Nols <jack0.magnus.core@gmail.com>
2021-07-15 20:36:57 +0800

    Added all files

---
+++ Network_Sockets_Client.py
@@ -0,0 +1,26 @@
+import socket
+
+SRV_ADDR = input("Type the server IP address: ")
+SRV_PORT = int(input("Type the server port: "))
+
+my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_sock.connect((SRV_ADDR, SRV_PORT))
+print("Connection established")
+
+message = input("Message to send: ")
+my_sock.sendall(message.encode())
+my_sock.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
